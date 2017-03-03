8423c874000f89a56a9be7cfdf7239855747de20
TomaszSzelachowski <up694239@myport.ac.uk>
2017-03-03 00:26:58 +0000

    Extra Classes
    Have two extra classes so that we don't repeat code within the system.
    
    ServerControl for things to do with the server and SeetingsControl for
    things which the user can do to change things with the app.

---
+++ src/pchecker/ServerControl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pchecker;
+
+/**
+ *
+ * @author Tomasz
+ */
+
+// Have everything we need to access the server on here so we don't repeat code
+// Make sure we have everything in one place to its easy to find and debug 
+// later on in the code
+
+public class ServerControl {
+    
+    
+public void accessServer(){
+// quick example
+}   
+}
